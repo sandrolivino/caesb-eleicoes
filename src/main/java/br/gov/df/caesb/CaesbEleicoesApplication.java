d9b899002b36a43ca75cc83a2c256e78e375818e
Sandro Livino <sandro.livino@gmail.com>
2023-04-04 14:10:59 -0300

    primeiro commit

---
+++ src/main/java/br/gov/df/caesb/CaesbEleicoesApplication.java
@@ -0,0 +1,13 @@
+package br.gov.df.caesb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaesbEleicoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaesbEleicoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/df/caesb/CaesbEleicoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.df.caesb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaesbEleicoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
